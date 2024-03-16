87cae21b0180802c86ae12bf4b410fb1ae546ff5
Maksim Yaskov <m.yaskov@astondevs.ru>
2024-03-16 20:11:42 +0300

    Add classes

---
+++ src/test/java/sandbox/jenkins/SandboxJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package sandbox.jenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void failTest() {
+        Assertions.assertTrue(false);
+    }
+
+}
